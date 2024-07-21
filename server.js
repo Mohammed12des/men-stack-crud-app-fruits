c59e80fa9dcfe91fdcb0d6a859a17cd63e3d8d43
Mohammed12des <hamood12des@gmail.com>
2024-07-21 15:29:49 +0300

    done

--- server.js
+++ server.js
@@ -1,26 +1,51 @@
 // Here is where we import modules
 // We begin by loading Express
 require('dotenv').config();
-const express = require("express");
-const morgan = require('morgan')
+const express = require('express');
+const morgan = require('morgan');
 
-//DATABASE
-require('./config/database')
+// DATABASE
+require('./config/database');
 
+const Fruit = require('./models/fruit.js');
 
 const app = express();
 
-//Middleware
+// MIDDLEWARE
 app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
 
-//Routs
+// ROUTES
 
+// Landing Page
+app.get('/', (req, res, next) => {
+  res.render('index.ejs');
+});
 
-//Landing page
-app.get('/',(req,res,next)=>{
-    res.render('index.ejs');
-})
+// Fruits
+app.get('/fruits/new', (req, res, next) => {
+  res.render('fruits/new.ejs');
+});
+
+app.post('/fruits', async (req, res, next) => {
+  
+  if (req.body.isReadyToEat === 'on') {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+
+  // insert the req body into the database
+  await Fruit.create(req.body);
+  res.redirect('/fruits');
+});
+
+app.get('/fruits', async (req, res, next) => {
+  const fruits = await Fruit.find();
+
+  res.render('fruits/index.ejs', { fruits });
+});
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
+  console.log('Listening on port 3000');
 });
